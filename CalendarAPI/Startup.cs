93b56b91f28acff75eddca1d408a7c6cd51870c9
Baran <baran34yildiz@gmail.com>
2019-05-03 11:39:29 +0300

    yeni

--- CalendarAPI/Startup.cs
+++ CalendarAPI/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-
 namespace CalendarAPI
 {
     public class Startup
