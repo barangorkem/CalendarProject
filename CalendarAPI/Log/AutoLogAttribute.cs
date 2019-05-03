7fc2aca25ba5c1dbac577898ff1c50d11cc442c5
Baran <baran34yildiz@gmail.com>
2019-05-03 12:02:11 +0300

    Log Dosyası Eklendi.

---
+++ CalendarAPI/Log/AutoLogAttribute.cs
@@ -0,0 +1,53 @@
+﻿using CalendarAPI.Response;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
+
+
+namespace CalendarAPI.Log
+{
+    public class AutoLogAttribute : TypeFilterAttribute
+    {
+        public AutoLogAttribute() : base(typeof(AutoLogActionFilterImpl))
+        {
+
+        }
+
+        private class AutoLogActionFilterImpl : IActionFilter
+        {
+            private readonly ILogger _logger;
+            public AutoLogActionFilterImpl(ILoggerFactory loggerFactory)
+            {
+                _logger = loggerFactory.CreateLogger<AutoLogAttribute>();
+            }
+
+            public void OnActionExecuting(ActionExecutingContext context)
+            {
+                //Gelen istekler yazılır.
+                LogWrite.LogMessage(context.HttpContext.Request.Path.ToString()); //İlk bağlantı olacağı zaman url çubuğundaki yol kaydedilir.
+            }
+
+            public void OnActionExecuted(ActionExecutedContext context)
+
+            {
+              
+
+
+                    var result = (ObjectResult)context.Result;
+                if(result.StatusCode==444)
+                {
+                    LogWrite.LogMessage(ErrorResponse.ERROR_REQUEST);
+                }
+                else
+                {
+                    LogWrite.LogMessage(result.Value.ToString());
+                }
+
+                //TODO: log body content and response as well
+
+            }
+
+        }
+    }
+}
+
---
+++ CalendarAPI/Log/LogWrite.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CalendarAPI.Log
+{
+    public class LogWrite
+    {
+        //private static string m_exePath = @"D:\Projelerim\CalendarProject-NetCore\CalendarAPI\Log";
+
+        private static string m_exePath = string.Empty;
+        public static void LogMessage(string logMessage)
+        {
+            m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (!System.IO.File.Exists(m_exePath + "\\" + "log.txt"))
+                System.IO.File.Create(m_exePath + "\\" + "log.txt");
+
+            try
+            {
+                using (StreamWriter w = System.IO.File.AppendText(m_exePath + "\\" + "log.txt"))
+                    AppendLog(logMessage, w);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        private static void AppendLog(string logMessage, TextWriter txtWriter)
+        {
+            try
+            {
+                txtWriter.Write("\r\nLog Entry : ");
+                txtWriter.WriteLine("{0} {1}:{2}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString(), logMessage);
+            
+                txtWriter.WriteLine("-------------------------------");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
